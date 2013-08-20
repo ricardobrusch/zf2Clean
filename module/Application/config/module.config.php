ff518fad4aec0af35cbdb5d71ed3dd87e33cb4af
Ricardo Brusch <brusch.ricardo@gmail.com>
2013-08-20 15:50:27 -0300

    Remoção de chave duplicada no array de configuração

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -60,11 +60,6 @@ return array(
             'Application\Controller\Index' => 'Application\Controller\IndexController'
         ),
     ),
-    'controllers' => array(
-        'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
-        ),
-    ),
     'view_manager' => array(
         'display_not_found_reason' => true,
         'display_exceptions'       => true,
